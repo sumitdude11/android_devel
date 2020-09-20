d009b12cc80459d352c6822ede2ac9a6996e909f
sumit rathore <sumitrathore1108@gmail.com>
2020-09-21 00:45:54 +0530

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+public class Member {
+    private String Name;
+   private String Typec;
+
+    public Member(String typec) {
+
+    }
+
+    public String getTypec() {
+        return Typec;
+    }
+
+    public void setTypec(String typec) {
+        Typec = typec;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Member()
+    {
+
+    }
+}
